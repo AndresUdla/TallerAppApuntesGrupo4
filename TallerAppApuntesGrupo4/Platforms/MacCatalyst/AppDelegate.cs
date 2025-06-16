bad0e27c38f1016397bf7a19521cc0c9770d20eb
Josue Mullo <josue.mullo@udla.edu.ec>
2025-06-16 10:39:30 -0500

    Add project files.

---
+++ TallerAppApuntesGrupo4/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TallerAppApuntesGrupo4
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
