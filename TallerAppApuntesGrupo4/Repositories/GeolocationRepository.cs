e6e4041fb51208d019602cbcd6909f9c270c4159
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-06-19 15:57:46 -0500

    Cambiamos el GeolocationRepository y agregamos el Weather repository

--- TallerAppApuntesGrupo4/Repositories/GeolocationRepository.cs
+++ TallerAppApuntesGrupo4/Repositories/GeolocationRepository.cs
@@ -8,5 +8,44 @@ namespace TallerAppApuntesGrupo4.Repositories
 {
     internal class GeolocationRepository
     {
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+        public async Task<Location> GetCurrentLocation()
+        {
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                    return location;
+            }
+            
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+            return new Location
+            {
+                Latitude = 46.9481,
+                Longitude = 7.4474,
+            };
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
+
     }
 }
