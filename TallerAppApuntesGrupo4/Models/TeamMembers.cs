a1e69ba33c6c94ef23bc4b39c1815548eb3da0d7
MartinBurga <mburga.xixv@gmail.com>
2025-06-20 02:31:23 -0500

    Creacion de un Modelo "TeamMember" y un ViewModel el cual cargara la informacion para mandarla al View.

---
+++ TallerAppApuntesGrupo4/Models/TeamMembers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerAppApuntesGrupo4.Models
+{
+    internal class TeamMembers
+    {
+        public string MemberName { get; set; }
+        public int MemberAge { get; set; }
+        public string MemberFavSport { get; set; }
+    }
+}
---
+++ TallerAppApuntesGrupo4/ViewModels/AboutViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using TallerAppApuntesGrupo4.Models;
+
+namespace TallerAppApuntesGrupo4.ViewModels
+{
+    internal class AboutViewModel
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string Message => "This app was created by Josue Mullo, Martin Burga & Santiago Pilamunga";
+
+        public ObservableCollection<TeamMembers> Members { get; set; } = new();
+
+        public AboutViewModel()
+        {
+            LoadMembers();
+        }
+
+        private void LoadMembers()
+        {
+            Members.Add(new TeamMembers
+            {
+                MemberName = "Josue Mullo",
+                MemberAge = 20,
+                MemberFavSport = "Football",
+            });
+            Members.Add(new TeamMembers
+            {
+                MemberName = "Martin Burga",
+                MemberAge = 20,
+                MemberFavSport = "Basketball",
+            });
+            Members.Add(new TeamMembers
+            {
+                MemberName = "Santiago Pilamunga",
+                MemberAge = 20,
+                MemberFavSport = "League of Legends",
+            });
+        }
+    }
+}
